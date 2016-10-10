a0b786e7d620805272b47fd412f5278f64d95ccd
Maxim Novikov <nes8bit@yandex-team.ru>
2016-10-10 15:11:51 +0500

    Garbage on heap

--- simple.cpp
+++ simple.cpp
@@ -1,12 +1,52 @@
-#include <string>
+#include <vector>
 #include <boost/python.hpp>
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 
 using namespace boost::python;
+namespace {
+    class JsonWriter
+    {
+        public:
+            //void add_number(double num) {
+                //this->numbers.push_back(num);
+            //}
+            const char * get_json_str() {
+                using namespace rapidjson;
+                Document doc;
+                doc.SetArray();
+                // Document::AllocatorType& allocator = doc.GetAllocator();
+                //StringBuffer buffer;
+                //Writer<StringBuffer> writer(buffer);
+                //doc.Reserve(kNumberType * this->numbers.size(), allocator);
+                //for (double item : this->numbers) {
+                    //doc.PushBack(item, allocator);
+                //}
+                //doc.Accept(writer);
+                return "Hello";
+                //return buffer.GetString();
+            }
+        //private:
+            //std::vector<double> numbers;
+    };
 
-std::string greet() { return "Hello, World!"; }
-int square(int number) { return number * number; }
+    const char* greet() { 
+        using namespace rapidjson;
+        Document doc;
+        doc.SetArray();
+        StringBuffer buffer;
+        Writer<StringBuffer> writer(buffer);
+        doc.Accept(writer);
+        return buffer.GetString();
+    }
+}
+
 BOOST_PYTHON_MODULE(simple)
 {
+    class_<JsonWriter>("JsonWriter")
+        //.def("add_number", &JsonWriter::add_number) 
+        .def("get_json_str", &JsonWriter::get_json_str)
+    ;
     def("greet", greet);
-    def("square", square);
 }
